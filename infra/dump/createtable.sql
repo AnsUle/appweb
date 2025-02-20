560b9b0d25272b1fe382a27663250fedc4bd423f
AnsUle <anna.suleyman@yahoo.fr>
2025-02-20 13:15:05 +0100

    modif jenkinsfile : ajout docker compose

--- infra/dump/createtable.sql
+++
@@ -1,7 +0,0 @@
-USE springboot;
-CREATE TABLE `Person` (
-    `id` int unsigned NOT NULL auto_increment,
-    `Lastname` varchar(50) default NULL,
-    `Firstname` varchar(50) default NULL,
-    PRIMARY KEY (`id`)
-);
